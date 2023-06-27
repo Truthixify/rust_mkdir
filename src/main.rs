e01cb23e894b8b3a832f20b0e005434c8da5e829
truthixify <truthixify@gmail.com>
2023-06-27 09:05:21 -0400

    updated main.rs to correct the crate name

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use colorful::Colorful;
 fn main() {
     let args : Vec<String> = std::env::args().collect();
 
-    let config = mkdir::Config::build(args.clone()).unwrap_or_else(|err| {
+    let config = rust_mkdir::Config::build(args.clone()).unwrap_or_else(|err| {
         eprintln!("{}", format!("⚠️ Problem parsing arguments: {}", err).gradient(Color::Red));
         eprintln!("{}", format!("ℹ Try 'mkdir --help' for more information.").gradient(Color::Green));
         std::process::exit(1);
     });
 
-    mkdir::run(config.flags, config.dir_list);
+    rust_mkdir::run(config.flags, config.dir_list);
 }
